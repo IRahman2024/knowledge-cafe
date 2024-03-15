6a36ec6ecb2a6b4d66a8c6d4fd7808807d659cb2
IRahman2024 <stormfuryultra784@gmail.com>
2024-03-16 04:41:03 +0600

    first commit

---
+++ src/component/header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className="text-4xl m-3 font-bold">Knowledge Cafe</h1>
+        </div>
+    );
+};
+
+export default Header;
